95958d331c12d392b2a75a36c945200392b8ae8a
Atyb. <180611249+atybdot@users.noreply.github.com>
2025-05-09 23:29:01 +0530

    feat: basic layout (#1)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <p className="font-mono">hello</p>
+    <div className="grid place-content-center min-h-svh w-full gap-2">
+      <p className="text-center">Home</p>
+      <Button asChild size={"sm"}>
+        <Link href={"/library"}> go to library</Link>
+      </Button>
     </div>
   );
 }
