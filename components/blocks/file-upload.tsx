7800426faf82156f11be61608fa5ac2957d74da1
atybdot <180611249+atybdot@users.noreply.github.com>
2025-05-12 06:06:14 +0530

    fix:conflicts

---
+++ components/blocks/file-upload.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import {
+  AlertCircleIcon,
+  PaperclipIcon,
+  PlusCircleIcon,
+  UploadIcon,
+  XIcon,
+} from "lucide-react";
+
+import { formatBytes, useFileUpload } from "@/hooks/use-file-upload";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+// Create some dummy initial files
+
+export default function Component() {
+  const maxSize = 10 * 1024 * 1024; // 10MB default
+
+  const [
+    { files, isDragging, errors },
+    {
+      handleDragEnter,
+      handleDragLeave,
+      handleDragOver,
+      handleDrop,
+      openFileDialog,
+      removeFile,
+      getInputProps,
+    },
+  ] = useFileUpload({
+    maxSize,
+    accept: ".epub",
+  });
+
+  const file = files[0];
+
+  return (
+    <div className="flex flex-col gap-2">
+      {/* Drop area */}
+      <div
+        role="button"
+        onClick={openFileDialog}
+        onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+        data-dragging={isDragging || undefined}
+        className={cn(
+          "border-input hover:bg-accent/50 data-[dragging=true]:bg-accent/50 has-[input:focus]:border-ring has-[input:focus]:ring-ring/50",
+          "flex min-h-40 h-full flex-col items-center justify-center p-4",
+          " transition-colors has-disabled:pointer-events-none has-disabled:opacity-50 has-[input:focus]:ring-[3px]"
+        )}
+      >
+        <input
+          {...getInputProps()}
+          className="sr-only"
+          aria-label="Upload file"
+          disabled={Boolean(file)}
+        />
+
+        <div className="flex flex-col items-center justify-center text-center">
+          <Button asChild variant={"ghost"} size={"icon"}>
+            <PlusCircleIcon className="size-9 text-muted-foreground" />
+          </Button>
+          <p className="text-sm">Add New Book </p>
+          <p className="text-muted-foreground text-xs">
+            Drag & drop or click to browse (max. {formatBytes(maxSize)})
+          </p>
+        </div>
+      </div>
+      {/* {errors.length > 0 && (
+        <div
+          className="text-destructive flex items-center gap-1 text-xs"
+          role="alert"
+        >
+          <AlertCircleIcon className="size-3 shrink-0" />
+          <span>{errors[0]}</span>
+        </div>
+      )} */}
+      {/* File list */}
+      {file && (
+        <div className="space-y-2">
+          <div
+            key={file.id}
+            className="flex items-center justify-between gap-2 rounded-xl border px-4 py-2"
+          >
+            <div className="flex items-center gap-3 overflow-hidden">
+              <PaperclipIcon
+                className="size-4 shrink-0 opacity-60"
+                aria-hidden="true"
+              />
+              <div className="min-w-0">
+                <p className="truncate text-[13px] font-medium">
+                  {file.file.name}
+                </p>
+              </div>
+            </div>
+
+            <Button
+              size="icon"
+              variant="ghost"
+              className="text-muted-foreground/80 hover:text-foreground -me-2 size-8 hover:bg-transparent"
+              onClick={() => removeFile(files[0]?.id)}
+              aria-label="Remove file"
+            >
+              <XIcon className="size-4" aria-hidden="true" />
+            </Button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+Component.displayName = "FileUploader";
