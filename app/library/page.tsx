8fef515748d7026fef9392ad36a8cf178b3a2f6e
atybdot <180611249+atybdot@users.noreply.github.com>
2025-05-14 12:31:38 +0530

    chore:breadbar

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -1,18 +1,10 @@
-"use client";
-import AddNewBook from "@/components/blocks/add-book";
+import BreadcrumbBar from "@/components/bread-crumb-bar";
 
-import { Button } from "@/components/ui/button";
-import { logBucks } from "@/actions/upload";
-import { useState } from "react";
-import { Input } from "@/components/ui/input";
-import { toast } from "sonner";
 export default function Page() {
-  const [file, setFile] = useState<File>();
   return (
-    <section className=" min-h-svh flex items-center justify-center">
-      <div className="w-1/3 m-auto">
-        <AddNewBook />
-      </div>
+    <section className=" min-h-svh grid grid-rows-[auto_1fr] w-full">
+      <BreadcrumbBar />
+      <div className="w-1/3 m-auto"></div>
     </section>
   );
 }
