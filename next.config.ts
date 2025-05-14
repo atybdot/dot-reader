ee54a36fb0b53d3ebe246b9d0af23c64a37b7355
atybdot <180611249+atybdot@users.noreply.github.com>
2025-05-14 09:29:28 +0530

    fix:uplaod limit to server action

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint:{
-    ignoreDuringBuilds: true
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-  typescript:{
-    ignoreBuildErrors : true
-  }
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "100mb",
+    },
+  },
 };
 
 export default nextConfig;
